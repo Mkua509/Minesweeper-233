8f2b330cbf9c0743e562c9167667a9de19c51197
Mkua509 <maternuskuang@gmail.com>
2025-04-18 18:54:40 +1200

    partially completed task 4

--- functions_minesweeper.py
+++ functions_minesweeper.py
@@ -30,20 +30,19 @@ def display_board(new_board):
     # loop through rows and cols 5x5
     for row in range(5):
         for col in range(5):
-            # index for rows 
+            # 2D to 3D
             i = row * 5 + col
             # fill in board string
             board = board + new_board[i] + " "
         board = board + "\n"
-    return board
+    print (board)
 
 '''
 Task 3: Inserting the mines
 A function called insert_mines that inserts mines to the board at specified postions.
 The mines should be represented by the X character
 Input 1: a list representing the board
-Input 2:list of lists representing each mine location. The first index in
-each nested list represents the row (0-4) and the second index represents the
+Input 2: list of lists representing each mine location. The first index in each nested list represents the row (0-4) and the second index represents the
 column (0-4)
 Output: No output
 Author: Maternus Kuang
@@ -60,8 +59,42 @@ def insert_mine(board, positions):
         # place mine at given location
         board[i] = "X"  
 
+
 # example usage
-my_board = initialise_board() 
-insert_mine(my_board, [[1, 2], [3, 4], [0, 0]]) 
-board_string = display_board(my_board)  
-print(board_string) 
+# my_board = initialise_board() 
+# insert_mine(my_board, [[1, 2], [3, 4], [0, 0]]) 
+# display_board(my_board)  # It prints the board inside
+
+'''
+Task 4: Counting Adjacent Mines
+A function count_adjacent_mines that counts the number of mines, X, adjacent (including diagonals!) to the selected row, col position
+Input 1: a list representing the board
+Input 2: an int reprersenting the row (0-4) of the square being checked for adjacent mines
+Input 3: an int representing the col (0-4) of the square being checked for adjacent mines
+Output: int representing the number (0-8) of adjacent mines
+Author: Maternus Kuang
+'''
+
+def count_adjacent_mines(board, rows, cols):
+    count = 0
+
+# loop through rows around cell
+    for r in range(rows - 1, rows + 2):
+        # loop thorugh cols around cell
+        for c in range(cols - 1, cols + 2):
+            # skip picked cell
+            if r == rows and c == cols:
+                continue
+
+            # withen boundarys
+            if 0 <= r < 5 and 0 <= c < 5:
+                index = r * 5 + c
+                if board[index] == "X":
+                    count += 1
+
+    return count
+
+board = initialise_board()
+insert_mine(board, [[0, 0], [1, 1], [2, 2]])
+display_board(board)
+print(count_adjacent_mines(board, 3, 2))  
